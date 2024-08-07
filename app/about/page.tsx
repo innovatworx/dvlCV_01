1680fb6eb120045ad1ceac1786217e4388dcc5f9
l Iz <n0v0_dvlpmt@outlook.com>
2024-08-07 11:50:41 -0500

    adjustments to the navbar for smaller screens & changes to the About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,27 +1,74 @@
 // pages/about.tsx
+"use client";
+
 import Navbar from '@/components/Navbar';
+import { AuroraBackground } from '@/components/ui/aurora-background';
+import { motion } from 'framer-motion';
+import React from 'react';
 
-export default function About() {
+const lines = [
+  "About Me",
+  ["Motivated IT professional with a strong foundation in cloud computing web development, and network security. Dedicated to optimizing system performance, deploying innovate solutions, and staying current with industry trends. Seeking opportunties as a"],
+  
+];
+
+const page = () => {
   return (
-    <div>
-      {/* <Navbar /> */}
-      <section className="h-screen flex flex-col justify-center items-center bg-gray-900 text-white p-8">
-        <h1 className="text-6xl mb-4 animate-fadeIn">About Me</h1>
-        <p className="text-2xl animate-fadeIn">I am a Web Developer with a passion for creating stunning websites.</p>
-        <p className='animate-fadeIn'>Motivated IT professional with a strong foundation in cloud computing,
-          web development, and network security. Dedicated to optimizing system
-          performance, deploying innovate solutions, and staying current with industry
-          trends. Seeking opportunties as a</p>
-
-        <div className="mt-8">
-          <h2 className="text-4xl">Work Experience</h2>
-          <p className="text-xl">Web Developer at XYZ Company</p>
-        </div>
-        <div className="mt-8">
-          <h2 className="text-4xl">Education</h2>
-          <p className="text-xl">B.Sc. in Computer Science</p>
-        </div>
-      </section>
-    </div>
+    <AuroraBackground>
+      <div className="relative flex flex-col gap-8 items-center justify-center px-24">
+        {lines.map((line, index) => (
+          <motion.div
+            key={index}
+            initial={{ opacity: 0.0, y: 40 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: index * 0.9, // Adjust the delay for staggering
+              duration: 0.8,
+              ease: "easeInOut",
+            }}
+            className={`${index === 0 ? 'text-4xl md:text-7xl mb-10' : 'text-xl md:text-2xl'
+              } font-bold dark:text-white text-center`}
+          >
+            {index === 0 ? (
+              line
+            ) : (
+              <div className="text-center">
+                <div className="flex items-center">
+                  <span className="mr-2 text-base md:text-lg"> {line[0]}</span>
+                </div>
+                <div className="ml-8 text-sm md:text-base">{line[1]}</div>
+              </div>
+            )}
+          </motion.div>
+        ))}
+      </div>
+    </AuroraBackground>
   );
-}
+};
+
+export default page
+
+// export default function About() {
+//   return (
+//     <div>
+//       <Navbar />
+//       <section className="h-screen flex flex-col justify-center items-center bg-gray-900 text-white p-8">
+//         <h1 className="text-6xl mb-4 animate-fadeIn">About Me</h1>
+//         <p className="text-2xl animate-fadeIn">I am a Web Developer with a passion for creating stunning websites.</p>
+//         <p className='animate-fadeIn'>Motivated IT professional with a strong foundation in cloud computing,
+//           web development, and network security. Dedicated to optimizing system
+//           performance, deploying innovate solutions, and staying current with industry
+//           trends. Seeking opportunties as a</p>
+
+//         <div className="mt-8">
+//           <h2 className="text-4xl">Work Experience</h2>
+//           <p className="text-xl">Web Developer at XYZ Company</p>
+//         </div>
+//         <div className="mt-8">
+//           <h2 className="text-4xl">Education</h2>
+//           <p className="text-xl">B.Sc. in Computer Science</p>
+//         </div>
+//       </section>
+//     </div>
+//   );
+// }
--- components/ui/navbar-menu.tsx
+++ components/ui/navbar-menu.tsx
@@ -40,7 +40,7 @@ export const MenuItem = ({
           transition={transition}
         >
           {active === item && (
-            <div className="absolute top-[calc(100%_+_1.2rem)] left-1/2 transform -translate-x-1/2 pt-4">
+            <div className="absolute top-[calc(100%_+_1.2rem)] left-1/2 transform -translate-x-1/2 pt-4 ">
               <motion.div
                 transition={transition}
                 layoutId="active" // layoutId ensures smooth animation
@@ -71,7 +71,7 @@ export const Menu = ({
   return (
     <nav
       onMouseLeave={() => setActive(null)} // resets the state
-      className="relative rounded-full boder border-transparent dark:bg-black dark:border-white/[0.2] bg-white shadow-input flex justify-center space-x-6 px-8 py-6 "
+      className="relative rounded-full border border-transparent dark:bg-black dark:border-white/[0.2] bg-white shadow-input flex justify-center space-x-6 px-8 py-6 md:px-4 md:py-3 md:space-x-3"
     >
       {children}
     </nav>
